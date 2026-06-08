eb6b51277c12263ef9931ecf2e8e3ae8d4eb8367
thinas1115 <thinas1115@gmail.com>
2026-06-08 11:05:16 +0000

    fix bugs

--- containers/lab/.devcontainer/entrypoint.sh
+++ containers/lab/.devcontainer/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set +e
 
+if [ -z "${CODE_SERVER_BIND_ADDR}" ]; then
+    CODE_SERVER_BIND_ADDR="0.0.0.0:5000"
+fi
+
+CONTAINERWSF="${CONTAINERWSF:-/lab}"
+
+code-server \
+    --bind-addr "${CODE_SERVER_BIND_ADDR}" \
+    --auth password \
+    --disable-telemetry \
+    --disable-update-check \
+    --disable-workspace-trust \
+    "${CONTAINERWSF}" &
+
 if ! ${CODESPACES:-false}; then
     if [ ${@+True} ]; then
         exec "$@"
@@ -12,15 +26,3 @@ if ! ${CODESPACES:-false}; then
         fi
     fi
 fi
-
-if [ -z "${CODE_SERVER_BIND_ADDR}" ]; then
-    CODE_SERVER_BIND_ADDR="0.0.0.0:5000"
-fi
-
-code-server \
-  --bind-addr ${CODE_SERVER_BIND_ADDR} \
-  --auth password \
-  --disable-telemetry \
-  --disable-update-check \
-  --disable-workspace-trust \
-  "${CONTAINERWSF}" &
